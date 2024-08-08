11cbb56ecae27d5775e618103140496bf04aed02
Otto Saarimaa <p86188@edu.sasky.fi>
2024-08-08 11:38:55 +0300

    Sivupohjat lisätty

---
+++ src/view/template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fi">
+  <head>
+    <title>OtsoArts - <?=$this->e($title)?></title>
+    <meta charset="UTF-8">    
+  </head>
+  <body>
+    <header>
+      <h1>OtsoArts</h1>
+    </header>
+    <section>
+      <?=$this->section('content')?>
+    </section>
+    <footer>
+      <hr>
+      <div>OtsoArts by Otto Saarimaa</div>
+    </footer>
+  </body>
+</html>
