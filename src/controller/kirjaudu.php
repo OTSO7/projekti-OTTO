5eab756a4c276a8f4d19cbc0ab3113a4252b4f68
Otto Saarimaa <p86188@edu.sasky.fi>
2024-08-09 14:40:52 +0300

    vahvistuslinkin aktivointi

--- src/controller/kirjaudu.php
+++ src/controller/kirjaudu.php
@@ -1,11 +1,10 @@
 <?php
 
-function tarkistaKirjautuminen($email="", $salasana="") {
+  function tarkistaKirjautuminen($email="", $salasana="") {
 
-    // Haetaan käyttäjän tiedot sen sähköpostiosoitteella.
+    // Haetaan käyttäjän tiedot sen sähköpostiosoitteella. 
     require_once(MODEL_DIR . 'henkilo.php');
     $tiedot = haeHenkilo($email);
-    $tiedot = array_shift($tiedot);
 
     // Tarkistetaan ensin löytyikö käyttäjä. Jos löytyi, niin
     // tarkistetaan täsmäävätkö salasanat.
@@ -36,6 +35,5 @@ function tarkistaKirjautuminen($email="", $salasana="") {
     session_destroy();
 
   }
-
 
 ?>
