3b8a96f8f95e629013b287b7b538f680faca8492
Otto Saarimaa <p86188@edu.sasky.fi>
2024-08-09 15:12:19 +0300

    uuden salasanan vaihtaminen

---
+++ src/view/reset_valmis.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <title>OtsoArts - <?=$this->e($title)?></title>
+    <meta charset="UTF-8">
+    <link href="styles/styles.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">
+                <a href="<?=BASEURL?>">OtsoArts</a>
+            </div>
+            <ul class="nav-links">
+                <li><?php if (isset($_SESSION['user'])) {
+            echo "<div class='username'>$_SESSION[user]</div>";
+            echo "<div><a href='logout'>Kirjaudu ulos</a></div>";
+          } else {
+            echo "<div><a href='kirjaudu'>Kirjaudu</a></div>";
+          } ?></li>
+                
+            </ul>
+        </nav>
+    </header>
+
+
+    <section class="hero-section">
+        <div class="hero-content">
+        <h1>Salasanasi on vaihdettu!</h1>
+
+<p>Voit käyttää kirjautumisessa uutta salasanaa.</p>
+</div>
+
+    </section>
