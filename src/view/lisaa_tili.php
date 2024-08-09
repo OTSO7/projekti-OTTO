90373e02d29680809297fcfd007eb3475603cc63
Otto Saarimaa <p86188@edu.sasky.fi>
2024-08-09 11:16:39 +0300

    lomakkeen tyylimääritykset

--- src/view/lisaa_tili.php
+++ src/view/lisaa_tili.php
@@ -26,27 +26,27 @@
         <div class="hero-content">
         <h1>Uuden tilin luonti</h1>
         <form action="" method="POST">
-  <div>
+  <div class="formi">
     <label for="nimi">Nimi:</label>
     <input id="nimi" type="text" name="nimi">
   </div>
-  <div>
+  <div class="formi">
     <label for="email">Sähköposti:</label>
     <input id="email" type="email" name="email">
   </div>
-  <div>
+  <div class="formi">
     <label for="discord">Discord-tunnus:</label>
     <input id="discord" type="text" name="discord">
   </div>
-  <div>
+  <div class="formi">
     <label for="salasana1">Salasana:</label>
     <input id="salasana1" type="password" name="salasana1">
   </div>
-  <div>
+  <div class="formi">
     <label for="salasana2">Salasana uudelleen:</label>
     <input id="salasana2" type="password" name="salasana2">
   </div>
-  <div>
+  <div class="nappi">
     <input type="submit" name="laheta" value="Luo tili">
   </div>
 </form>
